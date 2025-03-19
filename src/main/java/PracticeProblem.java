4e56c48a6e73cfc1f35fd833c738dc641cafe2c7
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-03-19 17:44:51 +0000

    Complete Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,73 @@
+import java.util.Arrays;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-		
 	}
 	
+public static int[] compareSearch(int[] array, int number){
+	Arrays.sort(array);
+int[] counters = new int[] {0, 0};
+int counterS = 0;
+int counterB = 0;
+for (int s = 0; s<array.length; s++){
+counterS++;
+if (number == array[s]){
+	s = array.length;
+}
+}
+int low =0;
+int high = array.length-1;
+int mid;
+while (low<=high){
+	mid = (low+high)/2;
+	counterB++;
+	if (number == array[mid]){
+		low = high+1;
+	}
+	else if (number > array[mid]){
+		low = mid+1;
+	}
+	else{
+		high = mid-1;
+	}
+}
+
+counters[0] = counterS;
+counters[1] = counterB;
+return counters;
+}
+
+public static int[] compareStringSearch(String[] array, String word){
+	Arrays.sort(array);
+	int[] counters = new int[] {0, 0};
+	int counterS = 0;
+	int counterB = 0;
+	for (int s = 0; s<array.length; s++){
+	counterS++;
+	if (word== array[s]){
+		s = array.length;
+	}
+	}
+	int low =0;
+	int high = array.length-1;
+	int mid;
+	while (low<=high){
+		mid = (low+high)/2;
+		counterB++;
+		if (word == array[mid]){
+			low = high+1;
+		}
+		else if (word.compareTo(array[mid])>0){
+			low = mid+1;
+		}
+		else{
+			high = mid-1;
+		}
+	}
+	
+	counters[0] = counterS;
+	counters[1] = counterB;
+	return counters;
+	}
 }
